db92165035c037fca042be84baf723d30c1bb218
kaypree <kaypree.hodges@gmail.com>
2024-03-14 21:33:32 -0400

    Initial Commit

---
+++ TinyBeansMovie/TinyBeansMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyBeansMovieApp.swift
+//  TinyBeansMovie
+//
+//  Created by Kaypree Hodges on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct TinyBeansMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
